7e50b3bc547d241b0435ab64cdde9b941e6235d8
lostmart <lostmart@hotmail.com>
2024-11-08 11:57:56 +0100

    new form from fetch

--- script.js
+++ script.js
@@ -72,9 +72,13 @@ function editModal(gameId) {
 	// console.log(gameId, gamesList)
 	// Trouvez le jeu en fonction de son identifiant
 	const result = gamesList.findIndex((game) => game.id === parseInt(gameId))
+	fetch("./form.html").then((data) => {
+		data.text().then((form) => {
+			modifyModal("Mode Edition", form)
+		})
+	})
 	// passer une image comme corps du modal
-	const modalBody = `<h4>ajoutez un formulaire pour modifier le jeu ici</h4>`
-	modifyModal("Mode Edition", modalBody)
+	// const modalBody = `<h4>ajoutez un formulaire pour modifier le jeu ici</h4>`
 }
 
 function modifyModal(modalTitle, modalBody) {
