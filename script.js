8f39ae35f383aec699b73175d157ed8165730cd8
lostmart <lostmart@hotmail.com>
2024-12-04 21:47:59 +0100

    new cars

--- script.js
+++ script.js
@@ -1,49 +1,23 @@
-const gamesList = [
-	{
-		title: "Tekken",
-		year: 1994,
-		imageUrl:
-			"https://cdn.dashfight.com/bcf6a9046a9ea4c1070d4aedb2981103c978a704.png",
-		id: 1,
-	},
-	{
-		title: "Minecraft",
-		year: 2009,
-		imageUrl:
-			"https://m.media-amazon.com/images/I/61smNbXSW1L._AC_UF1000,1000_QL80_.jpg",
-		id: 2,
-	},
-	{
-		title: "Elden Ring",
-		year: 2022,
-		imageUrl:
-			"https://pic.clubic.com/v1/images/1934271/raw?fit=smartCrop&width=1200&height=675&hash=e7519a9577a2b7291fa26880bb22bed6740836be",
-		id: 3,
-	},
-	{
-		title: "Street Fighter V",
-		year: 2015,
-		imageUrl:
-			"https://gaming-cdn.com/images/products/671/orig/street-fighter-v-pc-jeu-steam-cover.jpg?v=1662539255",
-		id: 4,
-	},
-	{
-		title: "Half Life 2",
-		year: 2004,
-		imageUrl:
-			"https://gaming-cdn.com/images/products/2284/orig/half-life-2-pc-mac-game-steam-cover.jpg?v=1650555068",
-		id: 5,
-	},
-	{
-		title: "Skyrim",
-		year: 2011,
-		imageUrl:
-			"https://gaming-cdn.com/images/products/146/orig/the-elder-scrolls-v-skyrim-pc-jeu-steam-europe-cover.jpg?v=1661270991",
-		id: 6,
-	},
-]
+// initialize variable
+let carsList
 
-writeDom()
+fetch("http://localhost:3000/api/cars", {
+	method: "GET",
+	headers: {
+		"x-api-key": "secret_phrase_here",
+		"Content-Type": "application/json",
+		Accept: "application/json",
+	},
+}).then((res) => {
+	if (!res.ok) {
+		console.log("something went very wrong !!!")
+	}
+	res.json().then((data) => {
+		console.log(data)
+		carsList = data
+		writeDom()
+	})
+})
 
 let editButtons = document.querySelectorAll(".edit")
 editButtons.forEach((btn) => {
@@ -60,12 +34,12 @@ viewButtons.forEach((btn) => {
 })
 
 function viewModal(gameId) {
-	// console.log(gameId, gamesList)
+	// console.log(gameId, carsList)
 	// Trouvez le jeu en fonction de son identifiant
-	const result = gamesList.findIndex((game) => game.id === parseInt(gameId))
+	const result = carsList.findIndex((game) => game.id === parseInt(gameId))
 	// passer une image comme corps du modal
-	const modalBody = `<img src="${gamesList[result].imageUrl}" alt="${gamesList[result].title}" class="img-fluid" />`
-	modifyModal(gamesList[result].title, modalBody)
+	const modalBody = `<img src="${carsList[result].imageUrl}" alt="${carsList[result].title}" class="img-fluid" />`
+	modifyModal(carsList[result].title, modalBody)
 	// edit footer
 	// Écrire dans le footer
 	document.querySelector(".modal-footer").innerHTML = `
@@ -77,12 +51,12 @@ function viewModal(gameId) {
 
 function editModal(gameId) {
 	// Trouvez le jeu en fonction de son identifiant
-	const result = gamesList.findIndex((game) => game.id === parseInt(gameId))
+	const result = carsList.findIndex((game) => game.id === parseInt(gameId))
 	// Injectez le formulaire dans le corps du modal
 	fetch("./form.html").then((data) => {
 		data.text().then((form) => {
 			// Modifiez le titre et le corps du modal
-			const selectedGame = gamesList[result]
+			const selectedGame = carsList[result]
 			modifyModal("Mode Edition", form)
 			modifyFom({
 				title: selectedGame.title,
@@ -120,16 +94,18 @@ function modifyModal(modalTitle, modalBody) {
 }
 
 function writeDom() {
-	gamesList.forEach((game) => {
+	if (!carsList) return
+
+	carsList.forEach((game) => {
 		const articleContainer = document.querySelector(".row")
 		articleContainer.innerHTML += `<article class="col">
 							<div class="card shadow-sm">
-								<img src="${game.imageUrl}" alt="${game.title}" class="card-img-top" />
+								<img src="${game.carImage}" alt="${game.carName}" class="card-img-top" />
 
 								<div class="card-body">
-								<h3 class="card-title">${game.title}</h3>
+								<h3 class="card-title">${game.carName}</h3>
 									<p class="card-text">
-										${game.year}
+										${game.carYear}
 									</p>
 									<div
 										class="d-flex justify-content-between align-items-center"
@@ -161,11 +137,11 @@ function writeDom() {
 
 function updateGames(title, year, imageUrl, gameId) {
 	// Trouvez le jeu en fonction de son identifiant
-	const index = gamesList.findIndex((game) => game.id === parseInt(gameId))
+	const index = carsList.findIndex((game) => game.id === parseInt(gameId))
 
-	gamesList[index].title = title
-	gamesList[index].year = year
-	gamesList[index].imageUrl = imageUrl
+	carsList[index].title = title
+	carsList[index].year = year
+	carsList[index].imageUrl = imageUrl
 	document.querySelector(".row").innerHTML = "" // Nous supprimons toutes les données des jeux dans le DOM.
 	writeDom()
 	editButtons = document.querySelectorAll(".edit")
