67314d822df6339212455397ab27f181ce00dc54
lostmart <lostmart@hotmail.com>
2024-12-07 11:55:11 +0100

    new log ins

--- login.js
+++ login.js
@@ -1,8 +1,72 @@
 const form = document.querySelector("form")
 
-console.log(form)
-
 form.addEventListener("submit", (e) => {
 	e.preventDefault()
-	console.log("run form submit ...")
+	const email = form.email.value
+	const userName = form.userName.value
+
+	// Validation du formulaire
+	try {
+		validateEmail(email)
+	} catch (error) {
+		showError(error)
+		return console.error(error)
+	}
+
+	try {
+		validateName(userName)
+	} catch (error) {
+		showError(error)
+		return console.error(error)
+	}
+
+	const formdata = {
+		email,
+		userName,
+	}
+
+	const url = "http://localhost:3000/api/users/login"
+	fetch(url, {
+		method: "POST",
+		headers: {
+			"x-api-key": "secret_phrase_here",
+			"Content-Type": "application/json",
+			Accept: "application/json",
+		},
+		body: JSON.stringify(formdata),
+	}).then((res) => console.log(res))
 })
+
+// input validation
+
+function validateName(input) {
+	if (!input || input.trim() === "") {
+		throw new Error("L'user name est vide !")
+	}
+
+	const alphanumericRegex = /^[a-zA-Z0-9]+$/
+	if (!alphanumericRegex.test(input)) {
+		throw new Error(
+			"L'entrée doit contenir uniquement des caractères alphanumériques."
+		)
+	}
+}
+
+function validateEmail(input) {
+	if (!input || input.trim() === "") {
+		throw new Error("L'email name est vide")
+	}
+
+	const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+	if (!emailRegex.test(input)) {
+		return "L'entrée doit contenir uniquement des caractères alphanumériques."
+	}
+}
+
+function showError(error) {
+	document.querySelector(".alert").innerText = error
+	document.querySelector(".alert").classList.remove("d-none")
+	setTimeout(() => {
+		document.querySelector(".alert").classList.add("d-none")
+	}, 2100)
+}
